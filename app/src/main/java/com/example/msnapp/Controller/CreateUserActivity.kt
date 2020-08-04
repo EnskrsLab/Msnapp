81ac761faaeac760eb069c4fefd54270890662d8
EnskrsLab <eneskaras@hotmail.com>
2020-08-04 22:44:54 +0300

    Lesson Creating A User completed

--- app/src/main/java/com/example/msnapp/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/msnapp/Controller/CreateUserActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import com.example.msnapp.R
 import com.example.msnapp.Services.AuthService
+import com.example.msnapp.Services.UserDataService
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -49,20 +50,25 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View) {
+
+        val userName = createUserNameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
         AuthService.registerUser(this, email, password) { registerSuccess ->
             if (registerSuccess) {
                 AuthService.loginUser(this, email, password) { loginSuccess ->
                     if (loginSuccess) {
-                        println(AuthService.authToken)
-                        println(AuthService.userEmail)
-
+                        AuthService.createUser(this, userName, email, userAvatar, avatarColor) { createSuccess ->
+                            if (createSuccess){
+                                println(UserDataService.avatarName)
+                                println(UserDataService.avatarColor)
+                                println(UserDataService.name)
+                                finish()
+                            }
+                        }
                     }
                 }
-
             }
-
         }
     }
 }
---
+++ app/src/main/java/com/example/msnapp/Services/UserDataService.kt
@@ -0,0 +1,11 @@
+package com.example.msnapp.Services
+
+object UserDataService {
+
+    var id = ""
+    var avatarColor = ""
+    var avatarName = ""
+    var email = ""
+    var name = ""
+
+}
